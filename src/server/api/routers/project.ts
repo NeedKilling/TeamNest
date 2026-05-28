528417cb3beb49f5b79c497dad1cf405ddfd58a1
Makar Kamardin <vsyakofazno@gmail.com>
2026-05-28 20:14:13 +0300

    added one api routers(projects)

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,6 +1,6 @@
 import { db } from "@/server/db";
 import { projects ,industries} from "@/server/db/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import Elysia from "elysia";
 import z from "zod/v4";
 
@@ -16,7 +16,10 @@ export const projectsRouter = new Elysia({
 })
 .get("/:id",async ({params})=>{
     const foundedProduct = await db.query.projects.findFirst({
-        where: eq(projects.id,params.id ),
+        where: and(
+            eq(projects.id, params.id ),
+            eq(projects.isDeleted, false ),
+        ) 
     })
 
     return foundedProduct ?? null
